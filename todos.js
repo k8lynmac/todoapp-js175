e0a8a2840ed88a6beb226d3b72c3b6a2de33250b
Katelyn <kcm34@pitt.edu>
2024-05-02 17:19:15 -0700

    Additional files added

---
+++ todos.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const morgan = require("morgan");
+const TodoList = require("./lib/todolist");
+
+const app = express();
+const host = "localhost";
+const port = 3000;
+
+let todoLists = require("./lib/seed-data");
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(morgan("common"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+// return the list of todo lists sorted by completion status and title.
+const sortTodoLists = lists => {
+  const compareByTitle = (todoListA, todoListB) => {
+    let titleA = todoListA.title.toLowerCase();
+    let titleB = todoListB.title.toLowerCase();
+
+    if (titleA < titleB) {
+      return -1;
+    } else if (titleA > titleB) {
+      return 1;
+    } else {
+      return 0;
+    }
+  };
+
+  let undone = lists.filter(todoList => !todoList.isDone());
+  let done   = lists.filter(todoList => todoList.isDone());
+  undone.sort(compareByTitle);
+  done.sort(compareByTitle);
+  return [].concat(undone, done);
+};
+
+app.get("/", (req, res) => {
+  res.redirect("/lists");
+});
+
+// Render new todo list page
+app.get("/lists/new", (req, res) => {
+  res.render("new-list");
+});
+
+// Render the list of todo lists
+app.get("/lists", (req, res) => {
+  res.render("lists", {
+    todoLists: sortTodoLists(todoLists),
+  });
+});
+
+// Create a new todo list
+app.post("/lists", (req, res) => {
+  let title = req.body.todoListTitle.trim();
+  if (title.length === 0) {
+    res.render("new-list", {
+      errorMessage: "A title was not provided.",
+    });
+  } else if (title.length > 100) {
+    res.render("new-list", {
+      errorMessage: "List title must be between 1 and 100 characters.",
+      todoListTitle: title,
+    });
+  } else if (todoLists.some(list => list.title === title)) {
+    res.render("new-list", {
+      errorMessage: "List title must be unique.",
+      todoListTitle: title,
+    });
+  } else {
+    todoLists.push(new TodoList(title));
+    res.redirect("/lists");
+  }
+});
+
+// Listener
+app.listen(port, host, () => {
+  console.log(`Todos is listening on port ${port} of ${host}!`);
+});
